3a6893604163a64e30965984d853769ebd889c7d
hahazexia <zhouzx@suirui.com>
2021-07-28 18:33:49 +0800

    keep-alive

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -42,6 +42,7 @@ function pruneCacheEntry (
 ) {
   const cached = cache[key]
   if (cached && (!current || cached.tag !== current.tag)) {
+    // 删除缓存 VNode 执行对应组件实例的 destory 钩子函数。
     cached.componentInstance.$destroy()
   }
   cache[key] = null
@@ -52,26 +53,27 @@ const patternTypes: Array<Function> = [String, RegExp, Array]
 
 export default {
   name: 'keep-alive',
-  abstract: true,
+  abstract: true, // 判断当前组件虚拟 dom 是否渲染成真实 dom 的关键
 
   props: {
-    include: patternTypes,
-    exclude: patternTypes,
-    max: [String, Number]
+    include: patternTypes, // 缓存白名单
+    exclude: patternTypes, // 缓存黑名单
+    max: [String, Number] // 缓存的组件实例数量上限
   },
 
   created () {
-    this.cache = Object.create(null)
-    this.keys = []
+    this.cache = Object.create(null) // 缓存虚拟 dom
+    this.keys = [] // 缓存的虚拟 dom 的 key 集合
   },
 
   destroyed () {
-    for (const key in this.cache) {
+    for (const key in this.cache) { // 删除所有的缓存
       pruneCacheEntry(this.cache, key, this.keys)
     }
   },
 
   mounted () {
+    // 实时监听黑白名单的变动
     this.$watch('include', val => {
       pruneCache(this, name => matches(val, name))
     })
@@ -81,6 +83,7 @@ export default {
   },
 
   render () {
+    // 通过 $slots 获取 keep-alive 包裹着的第一个子组件对象及其组件名
     const slot = this.$slots.default
     const vnode: VNode = getFirstComponentChild(slot)
     const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
@@ -94,6 +97,8 @@ export default {
         // excluded
         (exclude && name && matches(exclude, name))
       ) {
+        // 根据设定的黑白名单（如果有）进行条件匹配，决定是否缓存
+        // 这里的条件判断的是不匹配的情况，直接返回当前子组件的 vnode
         return vnode
       }
 
@@ -103,12 +108,15 @@ export default {
         // so cid alone is not enough (#3269)
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
+        // 如果 vnode 有 key ，直接使用 vnode.key，否则根据组件 cid 和 tag 生成缓存 key
+        // 在缓存对象中查找是否已缓存过该组件 vnode。如果存在，直接取出缓存值并更新该 key 在 this.keys 中的位置（更新 key 的位置是实现 LRU 置换策略的关键）
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
         remove(keys, key)
         keys.push(key)
       } else {
+        // 在 this.cache 对象中存储该组件 vnode 并保存 key 值，之后检查缓存的 vnode 数量是否超过 max 的设置值，超过则根据 LRU 置换策略删除最近最久未使用的 vnode（即是下标为 0 的那个 key）。
         cache[key] = vnode
         keys.push(key)
         // prune oldest entry
@@ -117,6 +125,7 @@ export default {
         }
       }
 
+      // 这一步很重要，将该组件 vnode 的 keepAlive 属性值设置为 true
       vnode.data.keepAlive = true
     }
     return vnode || (slot && slot[0])
